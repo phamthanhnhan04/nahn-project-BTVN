ddb788a3038fbdbb47b8b885075118250a2d84ec
Phạm Thành Nhân <146007487+phamthanhnhan04@users.noreply.github.com>
2024-01-24 08:05:06 +0700

    Add files via upload

---
+++ BTVN_csdl.py
@@ -0,0 +1,13 @@
+varA = 9
+varB = 10
+
+if type(varA) is str or type(varB) is str :
+    print ('varA là string')
+else:
+    print('varB là string')
+if varA > varB:
+        print("varA is larger than varB!")
+elif varA == varB:
+        print("varA is equal varB!") 
+else:
+            print("varA is smaller than varB!")
